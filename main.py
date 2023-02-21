c9a247ca4d305bdcd1e4052b830d39525364c425
yfpedraz <79946391+yfpedraz@users.noreply.github.com>
2023-02-21 09:47:38 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,70 +1,54 @@
 class Asiento:
 
-    def __init__(self):
-        self.color = ""
-        self.registro = 0
-        self.precio = 0
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
 
-    def cambiarColor(self, nuevo_color):
-        if nuevo_color in [ "rojo", "verde", "amarillo", "negro", "blanco"]:
-            self.color=nuevo_color
-
+    def cambiarColor(self, color):
+        if (color == "rojo" or  color == "verde" or color == "amarillo" or color == "negro" or color == "blanco"):
+            self.color = color
 
 class Motor:
 
-    def __init__(self):
-        self.numeroCilindros = 0
-        self.registro = 0
-        self.tipo = ""
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
 
-    def cambiarRegistro(self,registro):
-        if type(registro)==int:
-            self.registro=registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
 
-    def asignarTipo(self, tipo):
-        if tipo in ["gasolina","electrio"]:
-            self.tipo=tipo
-        
+    def asignarTipo (self, tipo):
+        if (tipo == "electrico" or tipo == "gasolina"):
+            self.tipo = tipo
 
-
+    
 class Auto:
+
     cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    
+    def cantidadAsientos (self):
+        numAsientos = 0
+        for asiento in self.asientos:
+            if (type(asiento) == Asiento):
+                numAsientos += 1
+        return numAsientos
 
-    def __init__(self):
-        self.modelo = ""
-        self.precio = 0
-        self.asientos = []
-        self.marca = ""
-        self.motor = Motor()
-        self.registro = 0
-        Auto.cantidadCreados += 1
-
-    def cantidadAsientos(self):
-        contar=0
-        for elem in self.asientos:
-            if type(elem)==Asiento:
-                contar += 1
-        return contar
-
+        
     def verificarIntegridad(self):
-        msg=["Las piezas no son originales","Auto original"]
-        verifica=all([i.registro==self.registro for i in self.asientos])
-        verifica=verifica and self.motor.registro==self.registro
-        return msg[verifica]
-
-
-"""
-car1=Auto()
-car1=Auto()
-print(Auto.cantidadCreados)
-print(car1.verificarIntegridad())
-car1.motor.cambiarRegistro(11)
-print(car1.verificarIntegridad())
-print(car1.cantidadAsientos())
-car1.asientos.append(Asiento())
-print(car1.cantidadAsientos())
-car1.asientos[0].cambiarColor("Azul")
-print(car1.asientos[0].color)
-car1.asientos[0].cambiarColor("negro")
-print(car1.asientos[0].color)
-"""
+        if (self.registro == self.motor.registro):
+            for asiento in self.asientos:
+                if (type(asiento) == Asiento):
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
